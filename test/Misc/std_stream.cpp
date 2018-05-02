410ab59c7e9fe38b94172ed844ea59f04af510a3
Benoit Blanchon <github@benoitblanchon.fr>
2018-05-02 09:49:12 +0200

    Improved std::stream tests

--- test/Misc/std_stream.cpp
+++ test/Misc/std_stream.cpp
@@ -6,60 +6,69 @@
 #include <catch.hpp>
 #include <sstream>
 
-TEST_CASE("std::stream") {
-  SECTION("JsonVariantFalse") {
-    std::ostringstream os;
+TEST_CASE("serialize to std::stream") {
+  DynamicJsonDocument doc;
+  std::ostringstream os;
+
+  SECTION("JsonVariant containing false") {
     JsonVariant variant = false;
+
     os << variant;
+
     REQUIRE("false" == os.str());
   }
 
-  SECTION("JsonVariantString") {
-    std::ostringstream os;
+  SECTION("JsonVariant containing string") {
     JsonVariant variant = "coucou";
+
     os << variant;
+
     REQUIRE("\"coucou\"" == os.str());
   }
 
   SECTION("JsonObject") {
-    std::ostringstream os;
-    DynamicJsonDocument doc;
     JsonObject& object = doc.to<JsonObject>();
     object["key"] = "value";
+
     os << object;
+
     REQUIRE("{\"key\":\"value\"}" == os.str());
   }
 
   SECTION("JsonObjectSubscript") {
-    std::ostringstream os;
-    DynamicJsonDocument doc;
     JsonObject& object = doc.to<JsonObject>();
     object["key"] = "value";
+
     os << object["key"];
+
     REQUIRE("\"value\"" == os.str());
   }
 
   SECTION("JsonArray") {
-    std::ostringstream os;
-    DynamicJsonDocument doc;
     JsonArray& array = doc.to<JsonArray>();
     array.add("value");
+
     os << array;
+
     REQUIRE("[\"value\"]" == os.str());
   }
 
   SECTION("JsonArraySubscript") {
-    std::ostringstream os;
-    DynamicJsonDocument doc;
     JsonArray& array = doc.to<JsonArray>();
     array.add("value");
+
     os << array[0];
+
     REQUIRE("\"value\"" == os.str());
   }
+}
 
-  SECTION("Should deserialize array") {
+TEST_CASE("deserialize from std::stream") {
+  DynamicJsonDocument doc;
+
+  SECTION("array") {
     std::istringstream json(" [ 42 /* comment */ ] ");
-    DynamicJsonDocument doc;
+
     JsonError err = deserializeJson(doc, json);
     JsonArray& arr = doc.as<JsonArray>();
 
@@ -68,9 +77,9 @@ TEST_CASE("std::stream") {
     REQUIRE(42 == arr[0]);
   }
 
-  SECTION("Should deserialize object") {
+  SECTION("object") {
     std::istringstream json(" { hello : world // comment\n }");
-    DynamicJsonDocument doc;
+
     JsonError err = deserializeJson(doc, json);
     JsonObject& obj = doc.as<JsonObject>();
 
@@ -81,15 +90,17 @@ TEST_CASE("std::stream") {
 
   SECTION("Should not read after the closing brace") {
     std::istringstream json("{}123");
-    DynamicJsonDocument doc;
+
     deserializeJson(doc, json);
+
     REQUIRE('1' == char(json.get()));
   }
 
   SECTION("Should not read after the closing bracket") {
     std::istringstream json("[]123");
-    DynamicJsonDocument doc;
+
     deserializeJson(doc, json);
+
     REQUIRE('1' == char(json.get()));
   }
 }
